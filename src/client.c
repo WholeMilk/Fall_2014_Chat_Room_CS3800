36ef94a0926c0ff5dbe6ae81ab72870e8ed819dd
WholeMilk <xiao.deng.1223@gmail.com>
2014-12-06 22:58:48 -0600

    Updated client.c

--- src/client.c
+++ src/client.c
@@ -6,9 +6,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/types.h>
-#include <sys/socket.h>  
-#include <netinet/in.h>  
-#include <netdb.h>       
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
 #include <pthread.h>
 #include <signal.h>
 
